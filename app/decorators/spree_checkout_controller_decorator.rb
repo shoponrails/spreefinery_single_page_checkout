aa06216984553d7654ec75d2dff8bc7a1e1ac7e4
Alexander Negoda <alexander.negoda@gmail.com>
2013-02-15 00:42:55 +0400

    spree view for single_page checkout

--- app/decorators/spree_checkout_controller_decorator.rb
+++ app/decorators/spree_checkout_controller_decorator.rb
@@ -10,8 +10,14 @@ Spree::CheckoutController.class_eval do
 
 
   def before_single_page
-    @order.bill_address ||= Spree::Address.default
-    @order.ship_address ||= Spree::Address.default
+
+    unless @order.bill_address or @order.ship_address
+      last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email)
+      preferred_bill_address, preferred_ship_address = current_refinery_user.bill_address, current_refinery_user.ship_address if current_refinery_user.respond_to?(:bill_address) && current_refinery_user.respond_to?(:ship_address)
+      @order.bill_address ||= preferred_bill_address || last_used_bill_address || Spree::Address.default
+      @order.ship_address ||= preferred_ship_address || last_used_ship_address || Spree::Address.default
+    end
+
     #@order.shipping_method ||= (@order.rate_hash.first && @order.rate_hash.first[:shipping_method])
     @order.payments.destroy_all if request.put?
   end
