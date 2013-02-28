af675ac728ddf84ce3838e1268ae7ea96657e319
Alexander Negoda <alexander.negoda@gmail.com>
2013-03-01 03:50:51 +0400

    fix checkout for current user

--- app/decorators/spree_checkout_controller_decorator.rb
+++ app/decorators/spree_checkout_controller_decorator.rb
@@ -9,6 +9,11 @@ Spree::CheckoutController.class_eval do
   end
 
   def edit
+
+     if @order.state.eql?('cart') and @order.email.present?
+       @order.next
+     end
+
      if ::Refinery::Setting.find_or_set(:single_page_checkout, true)
        if params[:state].present?
          session[:modify_state] = params[:state]
@@ -44,7 +49,27 @@ Spree::CheckoutController.class_eval do
     end
   end
 
+
   private
+
+  def before_address
+    if params[:bill_address] and params[:bill_address].eql?('bill_address_existing')
+      params[:order].delete(:bill_address_attributes)
+    end
+
+    if params[:ship_address] and params[:ship_address].eql?('ship_address_existing')
+      params[:order].delete(:ship_address_attributes)
+    end
+
+
+    return if @order.bill_address or @order.ship_address
+    last_used_bill_address, last_used_ship_address = Spree::Order.find_last_used_addresses(@order.email)
+    preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, spree_current_user.ship_address if spree_current_user.respond_to?(:bill_address) && spree_current_user.respond_to?(:ship_address)
+    @order.bill_address ||= preferred_bill_address || last_used_bill_address || Spree::Address.default
+    @order.ship_address ||= preferred_ship_address || last_used_ship_address || Spree::Address.default
+
+  end
+
 
   def ensure_valid_state
     unless skip_state_validation?
